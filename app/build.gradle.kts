31e43be02a19b306bea8fca56c5a98aa3f767985
mbp16 <phw1508@naver.com>
2024-07-04 11:34:59 +0900

    update version
    Signed-off-by: mbp16 <phw1508@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3.0"
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
