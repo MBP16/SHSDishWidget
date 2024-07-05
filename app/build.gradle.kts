97195d822f4ceb0bbfa4f1d68fe2cbf9ea3c411e
mbp16 <phw1508@naver.com>
2024-07-05 13:05:05 +0900

    update to version 1.4.7
    Signed-off-by: mbp16 <phw1508@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.4.5"
+        versionName = "1.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
